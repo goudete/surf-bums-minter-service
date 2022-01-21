c2af30153132f0ea16a7885c3c8beff766bc1713
goudete <goudetenrique@gmail.com>
2022-01-20 20:10:48 -0600

    slightly refactored createCharacter function

--- services/filesystem.js
+++ services/filesystem.js
@@ -32,12 +32,9 @@ const createCharacter = async (
         let attributesList = [];
 
         await Promise.all(dna.loadedElements).then(loadedElements => {
-            // create empty image
             ctx.clearRect(0, 0, width, height);
-            // draw a random background color
             drawBackground(ctx, width, height);
 
-            // draw each layer
             loadedElements.forEach(element => {
                 drawElement(ctx, element);
                 let selectedElement = element.layer.selectedElement;
@@ -46,15 +43,12 @@ const createCharacter = async (
                     rarity: selectedElement.rarity
                 });
             });
-            // add an image signature as the edition count to the top left of the image
             signImage(ctx, `#${editionCount}`);
-            // write the image to the output directory
         });
 
         let filename = editionCount.toString() + ".png";
         let filetype = "image/png";
 
-        // save locally as file
         fs.writeFileSync(`./output/${filename}`, canvas.toBuffer(filetype));
 
         console.log(`Created #${editionCount.toString()}`);
@@ -62,7 +56,7 @@ const createCharacter = async (
         const character = {
             editionCount,
             newDna: dna.newDna,
-            attributesList: attributesList
+            attributesList
         }
 
         return character;
@@ -85,24 +79,17 @@ const createDna = (
             newDna: null
         };
 
-        // holds which dna has already been used during generation and prepares dnaList object
         const dnaListByRarity = createDnaListByRarity(rarityWeights);
+        const rarity = getRarity(editionCount, editionSize);
 
-        // get rarity from to config to create NFT as
-        let rarity = getRarity(editionCount, editionSize);
-
-        // create unique Dna
         dna = {
             ...dna,
             newDna: createUniqueDna(layers, rarity, rarityWeights, dnaListByRarity)
         }
         dnaListByRarity[rarity].push(dna.newDna);
 
-        // propagate information about required layer contained within config into a mapping object
-        // = prepare for drawing
-        let layersResults = constructLayerToDna(dna.newDna, layers, rarity);
+        const layersResults = constructLayerToDna(dna.newDna, layers, rarity);
 
-        // load all images to be used by canvas
         layersResults.forEach(layer => {
             dna.loadedElements.push(loadLayerImg(layer));
         });
